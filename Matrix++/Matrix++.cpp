a301c4a8159e842e85bb7d40833ba4f04425b03e
exordzimka <exordzimka@gmail.com>
2020-10-03 17:22:19 +0300

    Добавьте файлы проекта.

---
+++ Matrix++/Matrix++.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <chrono>
+using namespace std;
+
+#define N 1000
+
+int matrix1[N][N];
+int matrix2[N][N];
+int matrixOfMulti[N][N];
+void SingleThreadMultiplication()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int summ = 0;
+            for (int k = 0; k < N; k++)
+            {
+                summ += matrix1[i][j] * matrix2[k][j];
+            }
+            matrixOfMulti[i][j] = summ;
+        }
+    }
+}
+
+void MultiThreadMultiplication()
+{
+    for (int i = 0; i < N; i++)
+    {
+        #pragma omp parallel for private(j)
+        for (int j = 0; j < N; j++)
+        {
+            int summ = 0;
+            for (int k = 0; k < N; k++)
+            {
+                summ += matrix1[i][j] * matrix2[k][j];
+            }
+            matrixOfMulti[i][j] = summ;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix1[i][j] = rand();
+            matrix2[i][j] = rand();
+        }
+    }
+    auto start = std::chrono::steady_clock::now();
+    //SingleThreadMultiplication();
+    MultiThreadMultiplication();
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+}
+
+
